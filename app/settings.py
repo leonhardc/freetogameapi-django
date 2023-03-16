ee7022c0aab6ef085a269f34ad5795f283963eb4
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-03-16 11:10:53 -0300

    adicionado o app game em settings.py

--- app/settings.py
+++ app/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'consumer.apps.ConsumerConfig',
+    'game.apps.GameConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
