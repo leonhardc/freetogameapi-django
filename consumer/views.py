17bef962d7f34c8e8d55e9e34afa3ebd3c315869
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-03-20 09:24:48 -0300

    update view - adicionado view do template index e inseridos primeiros códigos referentes aos filtros de games

--- consumer/views.py
+++ consumer/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.consumer import ConsumerAPI
+from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    consumer = ConsumerAPI()
+    games = consumer.consume()
+    return render(request, 'index.html', context={'games': games})
+
+def filter(request, filter):
+    print(filter)
+    consumer = ConsumerAPI()
+    games = consumer.consume()
+    return render(request, 'index.html', context={'games': games})
+    # plataforma = request.POST.get('plataforma')
+    # categoria = request.POST.get('categoria')
+    # ordem = request.POST.get('ordem')
+
+    # filters = {
+    #     'platform': plataforma, 
+    #     'category': categoria,
+    #     'sort': ordem
+    # }
+
+    # consumer = ConsumerAPI()
+    # games = consumer.consume(filter=filters)
+
+    # return render(request, 'index.html', context={'games': games})
+    pass
+    
+
+
