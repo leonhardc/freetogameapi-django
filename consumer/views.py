b8b652768f261dfc51c163ab1ecf5cc423920846
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-03-21 17:24:42 -0300

    resolvido problema de filtros com paginação

--- consumer/views.py
+++ consumer/views.py
@@ -2,48 +2,54 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from utils.consumer import ConsumerAPI
 from django.shortcuts import redirect
+from django.core.paginator import Paginator
+
+QUERY_DICT = {}
 
 # Create your views here.
 def index(request):
+    referer = request.META.get('HTTP_REFERER')
+    NUMBER_OF_GAMES = 12
     plataforma = request.GET.get('plataforma')
     categoria = request.GET.get('categoria')
     ordem = request.GET.get('ordem')
-    url = 'https://www.freetogame.com/api/games?'
-    
-    if plataforma=='' and categoria=='' and ordem=='':
-        consumer = ConsumerAPI()
-        games = consumer.consume()
-        return render(request, 'index.html', context={'games': games})
 
-    if plataforma:
-        url += f'platform={plataforma}'
-        if categoria:
-            url += f'&category={categoria}'
-        if ordem:
-            url += f'&sort={ordem}'
+    
+    if plataforma != '' or categoria != '' or ordem != '':
+        
+        url = 'https://www.freetogame.com/api/games?'   
+        
+        if plataforma:
+            url += f'platform={plataforma}'
+            if categoria:
+                url += f'&category={categoria}'
+            if ordem:
+                url += f'&sort={ordem}'
+        
+        elif categoria:
+            url += f'category={categoria}'
+            if ordem:
+                url += f'&sort={ordem}'
+
+        elif ordem:
+            url += f'sort={ordem}'
 
         consumer = ConsumerAPI()
         games = consumer.consume(url=url)
-        return render(request, 'index.html', context={'games': games})
-    
-    elif categoria:
-        url += f'category={categoria}'
-        if ordem:
-            url += f'&sort={ordem}'
+        paginator = Paginator(games, NUMBER_OF_GAMES)
 
-        consumer = ConsumerAPI()
-        games = consumer.consume(url=url)
-        return render(request, 'index.html', context={'games': games})
+        page = request.GET.get('page')
+        games_list = paginator.get_page(page)
 
-    elif ordem:
-        url += f'sort={ordem}'
+        return render(request, 'index.html', context={'games': games_list}) 
 
-        consumer = ConsumerAPI()
-        games = consumer.consume(url=url)
-        return render(request, 'index.html', context={'games': games})
-    
+
     consumer = ConsumerAPI()
     games = consumer.consume()
-    return render(request, 'index.html', context={'games': games})
+    paginator = Paginator(games, NUMBER_OF_GAMES)
+    page = request.GET.get('page')
+    games_list = paginator.get_page(page)
+
+    return render(request, 'index.html', context={'games': games_list})
 
 
